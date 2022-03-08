c4ba2c6c625f094f8e192fd11fed0fd69c02e208
Fernando Mumbach <fermuch@ayvu.net>
2022-03-08 12:11:28 -0300

    adding locks and special tags

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { SessionEvent } from "./events";
 import { currentLogin, getFrotaDoc, getString, set } from "./utils";
 
+let checklistUnlockTimer: NodeJS.Timeout | null = null;
 const LAST_LOGIN_KEY = 'LAST_LOGIN' as const;
 const LAST_LOGIN_AT_KEY = 'LAST_LOGIN_AT' as const;
 
@@ -12,7 +13,31 @@ type ReturnConfig = {
   enableReturn: false;
 }
 
-type Config = ReturnConfig & {
+type LockConfig = {
+  enableLock: true;
+  lockOutput: 'MONOFLOW_RELAY_1' | 'MONOFLOW_RELAY_2' | 'MONOFLOW_BUZ_1';
+  lockChecklistTime: number;
+} | {
+  enableLock: false;
+}
+
+type SpecialTag = {
+  tag: string;
+  action: "customChecklist";
+  customChecklistId: string;
+} | {
+  tag: string;
+  action: "omitChecklist";
+}
+
+type SpecialTagsConfig = {
+  enableSpecialTags: true;
+  specialTags: SpecialTag[];
+} | {
+  enableSpecialTags: false;
+}
+
+type Config = ReturnConfig & LockConfig & SpecialTagsConfig & {
   checklistId: string;
   checklistHours: number;
 }
@@ -22,12 +47,24 @@ function getConfig(): Config {
   if (!conf) {
     return {
       enableReturn: false,
+      enableLock: false,
+      enableSpecialTags: false,
       checklistHours: 8,
       checklistId: '',
     }
   }
 
   return conf;
+}
+
+function lock(doLock = true) {
+  const conf = getConfig();
+  if (!conf.enableLock) {
+    return;
+  }
+
+  const lockOutput = conf.lockOutput || 'MONOFLOW_RELAY_1';
+  env.setData(lockOutput, doLock);
 }
 
 messages.on('onInit', function() {
@@ -50,22 +87,19 @@ messages.on('onLogin', function(l) {
   // get settings
   const config = getConfig();
 
-  if (config.enableReturn) {
-    // check if user has returned
-    if (lastLogin === l && dateDiffHours < config.returnHours) {
-      platform.log('user has returned');
-      return env.setData('RETURN_VALUE', config.returnId);
+  const login = env.project?.logins.find((ll) => ll.key === l);
+  if (login && config.enableSpecialTags) {
+    for (const tag of config.specialTags) {
+      if (login.tags.includes(tag.tag)) {
+        if (tag.action === 'customChecklist') {
+          lock(false);
+          return env.setData('RETURN_VALUE', tag.customChecklistId);
+        } else if (tag.action === 'omitChecklist') {
+          lock(false);
+          return env.setData('RETURN_VALUE', '');
+        }
+      }
     }
-  }
-
-  if (lastLogin === l && dateDiffHours < config.checklistHours) {
-    platform.log('user has logged in before and is not overdue, skipping checklist');
-    return env.setData('RETURN_VALUE', '');
-  }
-
-  if (lastLogin !== l && config.checklistId) {
-    // we'll continue logic on onSubmit
-    return env.setData('RETURN_VALUE', config.checklistId);
   }
 
   // TODO:
@@ -81,19 +115,61 @@ messages.on('onLogin', function(l) {
   //   return;
   // }
 
+  if (config.enableReturn) {
+    // check if user has returned
+    if (lastLogin === l && dateDiffHours < config.returnHours) {
+      platform.log('user has returned');
+      lock(false);
+      return env.setData('RETURN_VALUE', config.returnId);
+    }
+  }
+
+  if (lastLogin === l && dateDiffHours < config.checklistHours) {
+    platform.log('user has logged in before and is not overdue, skipping checklist');
+    lock(false);
+    return env.setData('RETURN_VALUE', '');
+  }
+
+  // we'll continue logic for locks on onSubmit
+  lock(false);
   return env.setData('RETURN_VALUE', config.checklistId);
 });
+
+messages.on('onShowSubmit', (taskId, formId) => {
+  if (checklistUnlockTimer) {
+    clearTimeout(checklistUnlockTimer);
+    checklistUnlockTimer = null;
+  }
+
+  checklistUnlockTimer = setTimeout(() => {
+    platform.log('form not completed on time, locking checklist');
+    lock(true);
+  })
+})
 
 messages.on('onSubmit', (subm, taskId, formId) => {
   if (formId !== getConfig().checklistId) {
     return;
   }
 
+  if (checklistUnlockTimer) {
+    clearTimeout(checklistUnlockTimer);
+    checklistUnlockTimer = null;
+  }
+  lock(false);
   set(LAST_LOGIN_KEY, currentLogin());
 })
 
 messages.on('onLogout', (l) => {
+  lock(true);
   env.setData('LOGIN', '');
   getFrotaDoc()?.set('currentLogin', '');
   env.project?.saveEvent(new SessionEvent('end', l));
+})
+
+messages.on('onEnd', () => {
+  if (checklistUnlockTimer) {
+    clearTimeout(checklistUnlockTimer);
+    checklistUnlockTimer = null;
+  }
 })
