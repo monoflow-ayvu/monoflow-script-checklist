3b153ab7a4c032eee0cc29b7aa93ab19b04e0ad9
Fernando Mumbach <fermuch@ayvu.net>
2022-03-08 09:56:25 -0300

    porting most logic to plugin!

---
+++ src/events.ts
@@ -0,0 +1,23 @@
+import { BaseEvent } from "@fermuch/telematree/src/events";
+import { myID } from "./utils";
+
+export class SessionEvent extends BaseEvent {
+  kind = 'session';
+  type: 'start' | 'end';
+  userId: string;
+
+  constructor(type: 'start' | 'end', userId: string) {
+    super();
+
+    this.type = type;
+    this.userId = userId;
+  }
+
+  getData() {
+    return {
+      deviceId: myID(),
+      type: this.type,
+      userId: this.userId,
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,99 @@
-messages.on('onInit', function() {
-  platform.log('script started! (I am the script)');
-  platform.log('settings:');
-  platform.log(typeof settings?.());
-  platform.log(settings?.());
+import { SessionEvent } from "./events";
+import { currentLogin, getFrotaDoc, getString, set } from "./utils";
 
-  const {nome} = getSettings?.() as {nome?: string} || {nome: ''};
-  platform.log(`Hello, ${nome}!`);
+const LAST_LOGIN_KEY = 'LAST_LOGIN' as const;
+const LAST_LOGIN_AT_KEY = 'LAST_LOGIN_AT' as const;
+
+type ReturnConfig = {
+  enableReturn: true;
+  returnId: string;
+  returnHours: number;
+} | {
+  enableReturn: false;
+}
+
+type Config = ReturnConfig & {
+  checklistId: string;
+  checklistHours: number;
+}
+
+function getConfig(): Config {
+  const conf = getSettings?.() as Config;
+  if (!conf) {
+    return {
+      enableReturn: false,
+      checklistHours: 8,
+      checklistId: '',
+    }
+  }
+
+  return conf;
+}
+
+messages.on('onInit', function() {
+  platform.log('Checklist plugin initialized');
 });
+
+messages.on('onLogin', function(l) {
+  platform.log('executing login logic');
+
+  const lastLoginAt = new Date(getString(LAST_LOGIN_AT_KEY) || 0);
+  const dateDiffHours = Math.abs((new Date()).getTime() - lastLoginAt.getTime()) / 36e5;
+  const lastLogin = getString(LAST_LOGIN_KEY);
+
+  // store new login date
+  set(LAST_LOGIN_AT_KEY, (new Date()).toISOString());
+
+  getFrotaDoc().set('currentLogin', l);
+  env.project?.saveEvent(new SessionEvent('start', l));
+
+  // get settings
+  const config = getConfig();
+
+  if (config.enableReturn) {
+    // check if user has returned
+    if (lastLogin === l && dateDiffHours < config.returnHours) {
+      platform.log('user has returned');
+      return config.returnId;
+    }
+  }
+
+  if (lastLogin === l && dateDiffHours < config.checklistHours) {
+    platform.log('user has logged in before and is not overdue, skipping checklist');
+    return;
+  }
+
+  if (lastLogin !== l && config.checklistId) {
+    // we'll continue logic on onSubmit
+    return config.checklistId;
+  }
+
+  // TODO:
+  // const login = env.project?.logins.find((ll) => ll.key === l);
+  // if (login && (login.tags || []).includes('mecanico')) {
+  //   platform.log('mostrando checklist de mecánico');
+  //   return CHECKLIST_MECHANIC_FORM_ID;
+  // }
+
+  // TODO:
+  // if (login && (login.tags || []).includes('abastecedor')) {
+  //   platform.log('omitiendo checklist por abastecedor');
+  //   return;
+  // }
+
+  return config.checklistId;
+});
+
+messages.on('onSubmit', (subm, taskId, formId) => {
+  if (formId !== getConfig().checklistId) {
+    return;
+  }
+
+  set(LAST_LOGIN_KEY, currentLogin());
+})
+
+messages.on('onLogout', (l) => {
+  env.setData('LOGIN', '');
+  getFrotaDoc().set('currentLogin', '');
+  env.project?.saveEvent(new SessionEvent('end', l));
+})
