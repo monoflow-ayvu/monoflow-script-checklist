d46eb3ed7e2c551cfc7b547b52cfc0666943a70c
Fernando Mumbach <fermuch@ayvu.net>
2022-03-11 12:06:31 -0300

    fixs for webpack plugin compilation

--- webpack.config.js
+++ webpack.config.js
@@ -34,12 +34,12 @@ var babelOptions = {
 
 module.exports = {
   entry: entryFile,
-  target: ['es5'],
+  target: 'es5',
   // devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(t|j)s?$/,
         use: [
           {
             loader: 'babel-loader',
@@ -64,6 +64,26 @@ module.exports = {
     filename: 'bundle.js',
     path: distDir,
     chunkFormat: 'commonjs',
+    environment: {
+      // The environment supports arrow functions ('() => { ... }').
+      arrowFunction: false,
+      // The environment supports BigInt as literal (123n).
+      bigIntLiteral: false,
+      // The environment supports const and let for variable declarations.
+      const: false,
+      // The environment supports destructuring ('{ a, b } = obj').
+      destructuring: false,
+      // The environment supports an async import() function to import EcmaScript modules.
+      dynamicImport: false,
+      // The environment supports 'for of' iteration ('for (const x of array) { ... }').
+      forOf: false,
+      // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
+      module: false,
+      // The environment supports optional chaining ('obj?.a' or 'obj?.()').
+      optionalChaining: false,
+      // The environment supports template literals.
+      templateLiteral: false,
+    },
   },
   optimization: {
     usedExports: true,
