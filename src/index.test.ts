2d07cfb0c8b4b4e475cb197f93fe2048cea40310
Fernando Mumbach <fermuch@ayvu.net>
2022-03-17 17:17:16 -0300

    adding basic structure for tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -18,24 +18,54 @@ describe("onInit", () => {
     messages.emit('onInit');
   });
 
-  it('prints "Hello, default name!"', () => {
-    const log = jest.fn();
-    platform.log = log;
+  xdescribe('onLogin', () => {
+    it('onLogin sets IS_DEVICE_LOCKED', () => {});
+    it('onLogin does not go through if device is locked', () => {});
+    it('onLogin sets currentLogin on frota doc', () => {});
+    it('onLogin saves a login event', () => {});
+    it('unlocks device for a given time to complete the checklist', () => {});
 
-    loadScript();
+    xdescribe('onLogin is a supervisor', () => {
+      it('onLogin only lets a supervisor pass if device is locked', () => {});
+      it('onLogin sets LAST_LOGIN_AT if device is unlocked by supervisor', () => {});
+      it('onLogin passes without checklist if device is unlocked by supervisor', () => {});
+    });
 
-    messages.emit('onInit');
-    expect(log).toHaveBeenCalledWith('Hello, default name!');
+    xdescribe('special tags', () => {
+      it('shows custom checklist for customChecklist tag', () => {});
+      it('omitChecklist tag skips checklist', () => {});
+      it('always sets LAST_LOGIN_AT', () => {});
+    });
+
+    xdescribe('enableReturn', () => {
+      it('if same user returns before time limit, passes without checklist', () => {});
+      it('continues to the checklist for returnId', () => {});
+    });
   });
 
-  it('prints "Hello, custom name!" if given config', () => {
-    const log = jest.fn();
-    platform.log = log;
-    getSettings = () => ({ name: 'custom name' });
-
-    loadScript();
-
-    messages.emit('onInit');
-    expect(log).toHaveBeenCalledWith('Hello, custom name!');
+  xdescribe('onShowSubmit', () => {
+    it('locks if more time than lockChecklistTime passes and enableLock is enabled', () => {});
   });
+
+  xdescribe('onSubmit', () => {
+    it('sets LAST_LOGIN_AT if formId is returnId or checklistId', () => {});
+    it('does not continue if ID is not checklistId', () => {});
+    it('disables checklistUnlockTimer if checklist is submitted', () => {});
+    it('unlocks the machine under normal conditions', () => {});
+
+    xdescribe('checklistQuestionsEnabled', () => {
+      it('keeps locked if keepLocked is true', () => {});
+      it('goes into critical mode if critical is true', () => {});
+    });
+  });
+
+  xdescribe('onLogout', () => {
+    it('unsets LOGIN env var', () => {});
+    it('unsets currentLogin on frota doc', () => {});
+    it('saves a logout event', () => {});
+  });
+
+  xdescribe('onEnd', () => {
+    it('cleans checklistUnlockTimer if there is one', () => {});
+  })
 });
