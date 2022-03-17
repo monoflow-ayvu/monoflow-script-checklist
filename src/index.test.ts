f4a9e6543b3d46ede0d3fcb020e668f83de65664
Fernando Mumbach <fermuch@ayvu.net>
2022-03-17 17:38:04 -0300

    adding tests for supervisors

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,4 @@
+import * as MonoUtils from '@fermuch/monoutils';
 const read = require('fs').readFileSync;
 const join = require('path').join;
 
@@ -11,6 +12,7 @@ describe("onInit", () => {
   // clean listeners
   afterEach(() => {
     messages.removeAllListeners();
+    MonoUtils.storage.set('IS_DEVICE_LOCKED', false);
   });
 
   it('runs without errors', () => {
@@ -18,54 +20,182 @@ describe("onInit", () => {
     messages.emit('onInit');
   });
 
-  xdescribe('onLogin', () => {
-    it('onLogin sets IS_DEVICE_LOCKED', () => {});
-    it('onLogin does not go through if device is locked', () => {});
-    it('onLogin sets currentLogin on frota doc', () => {});
-    it('onLogin saves a login event', () => {});
-    it('unlocks device for a given time to complete the checklist', () => {});
+  describe('onLogin', () => {
+    it('onLogin sets IS_DEVICE_LOCKED', () => {
+      loadScript();
+      messages.emit('onInit');
+      messages.emit('onLogin', '123', '');
+      expect(env.data.IS_DEVICE_LOCKED).toBe(false);
+    });
+    
+    it('onLogin does not go through if device is locked', () => {
+      loadScript();
+      messages.emit('onInit');
+      MonoUtils.storage.set('IS_DEVICE_LOCKED', true);
+      messages.emit('onLogin', '123', '');
+      expect(env.data.RETURN_VALUE).toStrictEqual({ error: 'Supervisor requerido.' });
+    });
 
-    xdescribe('onLogin is a supervisor', () => {
-      it('onLogin only lets a supervisor pass if device is locked', () => {});
-      it('onLogin sets LAST_LOGIN_AT if device is unlocked by supervisor', () => {});
-      it('onLogin passes without checklist if device is unlocked by supervisor', () => {});
+    it('onLogin sets currentLogin on frota doc', () => {
+      const colStore = {} as Record<any, any>;
+      const mockCol = {
+        get() {
+          return {
+            data: colStore,
+            get: (k: string) => colStore[k],
+            set: (k: string, v: any) => (colStore[k] = v),
+          }
+        }
+      };
+      (env.project as any) = {
+        collectionsManager: {
+          ensureExists: () => mockCol,
+        },
+        saveEvent: jest.fn(),
+        logins: [],
+      };
+      loadScript();
+      messages.emit('onInit');
+      messages.emit('onLogin', '123', '');
+
+      expect(colStore.currentLogin).toBe('123');
+    });
+
+    it('onLogin saves a login event', () => {
+      const colStore = {} as Record<any, any>;
+      const mockCol = {
+        get() {
+          return {
+            data: colStore,
+            get: (k: string) => colStore[k],
+            set: (k: string, v: any) => (colStore[k] = v),
+          }
+        }
+      };
+      (env.project as any) = {
+        collectionsManager: {
+          ensureExists: () => mockCol,
+        },
+        saveEvent: jest.fn(),
+        logins: [],
+      };
+      loadScript();
+      messages.emit('onInit');
+      messages.emit('onLogin', '123', '');
+
+      expect(env.project.saveEvent).toHaveBeenCalledTimes(1);
+    });
+
+    it('unlocks device so the checklist decides if we need to block or not', () => {
+      loadScript();
+      messages.emit('onInit');
+      messages.emit('onLogin', '123', '');
+      expect(env.data.IS_DEVICE_LOCKED).toBe(false);
+    });
+
+    describe('onLogin is a supervisor', () => {
+      it('onLogin only lets a supervisor pass if device is locked', () => {
+        const colStore = {} as Record<any, any>;
+        const mockCol = {
+          get() {
+            return {
+              data: colStore,
+              get: (k: string) => colStore[k],
+              set: (k: string, v: any) => (colStore[k] = v),
+            }
+          }
+        };
+        (env.project as any) = {
+          collectionsManager: {
+            ensureExists: () => mockCol,
+          },
+          saveEvent: jest.fn(),
+          logins: [{key: '123', tags: ['supervisor']}],
+        };
+        getSettings = () => ({
+          enableSpecialTags: true,
+          specialTags: [{tag: 'supervisor', action: 'supervisor'}],
+        });
+
+        loadScript();
+        messages.emit('onInit');
+        MonoUtils.storage.set('IS_DEVICE_LOCKED', true);
+        messages.emit('onLogin', '123', '');
+        expect(MonoUtils.storage.getBoolean('IS_DEVICE_LOCKED')).toBe(false);
+        expect(env.data.RETURN_VALUE).toBe('');
+        expect(MonoUtils.storage.getString('LAST_LOGIN_AT')).toBeTruthy();
+      });
+
+      it('onLogin passes without checklist if device is unlocked by supervisor', () => {
+        const colStore = {} as Record<any, any>;
+        const mockCol = {
+          get() {
+            return {
+              data: colStore,
+              get: (k: string) => colStore[k],
+              set: (k: string, v: any) => (colStore[k] = v),
+            }
+          }
+        };
+        (env.project as any) = {
+          collectionsManager: {
+            ensureExists: () => mockCol,
+          },
+          saveEvent: jest.fn(),
+          logins: [{key: '123', tags: ['supervisor']}],
+        };
+        getSettings = () => ({
+          enableSpecialTags: true,
+          specialTags: [{tag: 'supervisor', action: 'supervisor'}],
+        });
+
+        loadScript();
+        messages.emit('onInit');
+        MonoUtils.storage.set('IS_DEVICE_LOCKED', true);
+        messages.emit('onLogin', '123', '');
+        expect(MonoUtils.storage.getBoolean('IS_DEVICE_LOCKED')).toBe(false);
+        expect(env.data.RETURN_VALUE).toBe('');
+        expect(MonoUtils.storage.getString('LAST_LOGIN_AT')).toBeTruthy();
+
+        expect(env.data.RETURN_VALUE).toBe('');
+      });
     });
 
     xdescribe('special tags', () => {
-      it('shows custom checklist for customChecklist tag', () => {});
-      it('omitChecklist tag skips checklist', () => {});
-      it('always sets LAST_LOGIN_AT', () => {});
+      xit('shows custom checklist for customChecklist tag', () => { });
+      xit('omitChecklist tag skips checklist', () => { });
+      xit('always sets LAST_LOGIN_AT', () => { });
     });
 
     xdescribe('enableReturn', () => {
-      it('if same user returns before time limit, passes without checklist', () => {});
-      it('continues to the checklist for returnId', () => {});
+      xit('if same user returns before time limit, passes without checklist', () => { });
+      xit('continues to the checklist for returnId', () => { });
     });
   });
 
   xdescribe('onShowSubmit', () => {
-    it('locks if more time than lockChecklistTime passes and enableLock is enabled', () => {});
+    xit('locks if more time than lockChecklistTime passes and enableLock is enabled', () => { });
   });
 
   xdescribe('onSubmit', () => {
-    it('sets LAST_LOGIN_AT if formId is returnId or checklistId', () => {});
-    it('does not continue if ID is not checklistId', () => {});
-    it('disables checklistUnlockTimer if checklist is submitted', () => {});
-    it('unlocks the machine under normal conditions', () => {});
+    xit('sets LAST_LOGIN_AT if formId is returnId or checklistId', () => { });
+    xit('does not continue if ID is not checklistId', () => { });
+    xit('disables checklistUnlockTimer if checklist is submitted', () => { });
+    xit('unlocks the machine under normal conditions', () => { });
 
     xdescribe('checklistQuestionsEnabled', () => {
-      it('keeps locked if keepLocked is true', () => {});
-      it('goes into critical mode if critical is true', () => {});
+      xit('keeps locked if keepLocked is true', () => { });
+      xit('goes into critical mode if critical is true', () => { });
     });
   });
 
   xdescribe('onLogout', () => {
-    it('unsets LOGIN env var', () => {});
-    it('unsets currentLogin on frota doc', () => {});
-    it('saves a logout event', () => {});
+    xit('unsets LOGIN env var', () => { });
+    xit('unsets currentLogin on frota doc', () => { });
+    xit('saves a logout event', () => { });
   });
 
   xdescribe('onEnd', () => {
-    it('cleans checklistUnlockTimer if there is one', () => {});
+    xit('cleans checklistUnlockTimer if there is one', () => { });
   })
 });
